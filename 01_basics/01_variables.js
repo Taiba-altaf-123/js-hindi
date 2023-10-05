f1bbe85c7a8800d2b50e4b378284b3d5ea06f35b
Taiba-altaf-123 <127793088+Taiba-altaf-123@users.noreply.github.com>
2023-10-05 16:05:45 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=12435
+let accountEmail="taiba@gmail.com"
+var accountPassword="taiba"
+accountCity="Mumbai"
+let accountName
+
+//accountId=67849
+console.log(accountId)
+
+accountEmail="abc@gmail.com"
+accountPassword="abc"
+accountCity="Jaipur"
+
+/*Never use var keyword to declare variable because of its block scope issue */
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountName])
