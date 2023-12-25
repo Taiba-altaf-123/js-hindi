3631c082ea205e63cd0a8ed78f2d062b0b80bb4f
Taiba-altaf-123 <127793088+Taiba-altaf-123@users.noreply.github.com>
2023-12-25 13:02:14 +0000

    objects basics

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+// singleton
+//Objects can be created using object constructor and object literal
+//Object constructor 
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+//this is used when you want to access the properties of the same object on which you are operating
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
