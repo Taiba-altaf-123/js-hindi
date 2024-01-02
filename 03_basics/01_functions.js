591b332dcd9a31345d80e455de40eb55e6170822
Taiba-altaf-123 <127793088+Taiba-altaf-123@users.noreply.github.com>
2024-01-02 16:19:04 +0000

    function basics

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,73 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+//sayMyName  This gives the reference of the function
+// sayMyName() This executes the function
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){   //This line checks whether username is empty 
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+// (...)This is sometimes called a rest operator and sometimes spread operator,depending upon the the variable.
+//Jab arrays par (...) use ho to spread operator bolte hai aur Jab variables(numbers ya strings ) par use ho to rest operator bolte hai
+//Spread operators allow us to expand an array or object into its individual elements, while rest operators allow us to condense multiple elements into a single array or object. 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
